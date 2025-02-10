9c55b0bafeed9afeccdb7cf295334720a630d346
Leonardo Scalabrini <leonardo_scalabrini@hotmail.com>
2025-02-09 23:55:48 -0300

    Added cucumber

---
+++ src/test/java/com/exercise/RunCucumberTest.java
@@ -0,0 +1,14 @@
+package com.exercise;
+
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com.exercise")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.exercise")
+public class RunCucumberTest {
+}
